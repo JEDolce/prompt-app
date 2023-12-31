be8b204ab64fe26a3944390b35b4a966ac6ae6bf
juandolce <juandolce20@gmail.com>
2023-12-31 17:48:12 -0300

    update and delete prompts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -58,7 +58,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 className="font-inter text-sm blue_gradient cursor-pointer"
                 onClick={() => handleTagClick && handleTagClick(post.tag)}
             >
-                {post.tag}
+                #{post.tag}
             </p>
 
             {/* Si el usuario actual es el creador del post y si esta en la pagina de profile */}
