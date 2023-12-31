7cccbed661ca9ceab2c68c2527c0cee4f746a2f8
juandolce <juandolce20@gmail.com>
2023-12-31 02:29:37 -0300

    Feed and show prompts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+"use client"
 
-const PromptCard = () => {
+import { useState } from "react"
+import Image from 'next/image'
+import { useSession } from "next-auth/react"
+import { usePathname, useRouter } from "next/navigation"
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    const [copied, setCopied] = useState("");
+
+    /* Funcionalidad para copiar a portapapeles */
+    const handleCopy = () => {
+        setCopied(post.prompt);
+        navigator.clipboard.writeText(post.prompt);
+        setTimeout(() => setCopied(""), 3000);
+    };
+
     return (
-        <div>PromptCard</div>
+        <div className="prompt_card">
+            <div className="flex justify-between items-start gap-5">
+                <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+                    <Image
+                        src={post.creator.image}
+                        alt="user image"
+                        width={40}
+                        height={40}
+                        className="rounded-full object-contain"
+                    />
+
+                    <div className="flex flex-col">
+                        <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+                        <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+                    </div>
+                </div>
+
+                <div
+                    className="copy_btn shadow-[inset_10px_-50px_94px_0_rgb(199, 199, 199, 0.2)]"
+                    onClick={handleCopy}
+                >
+                    <Image
+                        src={copied === post.prompt
+                            ? '/assets/icons/tick.svg'
+                            : '/assets/icons/copy.svg'
+                        }
+                        width={18}
+                        height={18}
+                    />
+                </div>
+            </div>
+
+            <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+            <p
+                className="font-inter text-sm blue_gradient cursor-pointer"
+                onClick={() => handleTagClick && handleTagClick(post.tag)}
+            >
+                {post.tag}
+            </p>
+        </div>
+
     )
 }
 
